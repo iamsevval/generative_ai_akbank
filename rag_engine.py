3d4896e51aa3f018700e51f073f5e6d93b86cb39
Şevval Arslan <sevvalarslan003@gmail.com>
2026-05-31 03:28:11 +0300

    Fix: Update LangChain text_splitter import path for latest version compatibility

--- rag_engine.py
+++ rag_engine.py
@@ -2,7 +2,7 @@ import os
 from langchain_community.document_loaders import DataFrameLoader
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
